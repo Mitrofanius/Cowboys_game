07ba8efdd6485b8607075c7cce02d048acb5d220
Litvin, Dmitrii <litvidmi@fel.cvut.cz>
2021-05-26 18:44:22 +0200

    Added new colours for background and menu tiles

--- Cowboys/draw_main_menu.c
+++ Cowboys/draw_main_menu.c
@@ -7,22 +7,20 @@ void draw_main_menu(unsigned char *parlcd_mem_base, unsigned short *frame_buffer
     /* Background */
     for (i = 0; i < 320 * 480; i++)
     {
-        frame_buffer[i] = 0xCCCE;
+        frame_buffer[i] = 0xBC06;
     }
 
     /* 1 PLAYER rectangle */
-    draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 22, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x49A5);
+    draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 22, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x9B01);
     /* 2 PLAYERS rectangle */
-    draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 98, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x49A5);
+    draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 98, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x8AC1);
     /* STTINGS rectangle */
     draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 173, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x49A5);
     /* EXIT rectangle */
     draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 247, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x49A5);
 
-
     /* CHOICE rectangle */
     draw_rectangle(frame_buffer, REGTANGL_LEFT_UP_X, 22 + *choice * 75, REGTANGLE_WIDTH, REGTANGLE_HEIGHT, REGTANGLE_FRAME_HEIGHT, 0, 0x9CF3);
-
 
     /* 1 PLAYER label */
     draw_char(frame_buffer, font_descriptor, 88, 25, '1', CHAR_SCALE, 0xFFFF);
@@ -58,7 +56,6 @@ void draw_main_menu(unsigned char *parlcd_mem_base, unsigned short *frame_buffer
     draw_char(frame_buffer, font_descriptor, 216, 250, 'X', CHAR_SCALE, 0xFFFF);
     draw_char(frame_buffer, font_descriptor, 260, 250, 'I', CHAR_SCALE, 0xFFFF);
     draw_char(frame_buffer, font_descriptor, 286, 250, 'T', CHAR_SCALE, 0xFFFF);
-
 
     /* Sends info to screen */
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
--- Cowboys/draw_two_players_game.c
+++ Cowboys/draw_two_players_game.c
@@ -7,12 +7,12 @@ void draw_two_players_game(unsigned char *parlcd_mem_base, unsigned short *frame
     /* Background */
     for (i = 0; i < 320 * 480; i++)
     {
-        frame_buffer[i] = 0xCCCE;
+        frame_buffer[i] = 0xBC06;
     }
 
     draw_cowboy(parlcd_mem_base, frame_buffer, font_descriptor, &(game_map->cowboy_left), 3);
     draw_cowboy(parlcd_mem_base, frame_buffer, font_descriptor, &(game_map->cowboy_right), 3);
-    
+
     /* Sends info to screen */
     parlcd_write_cmd(parlcd_mem_base, 0x2c);
     for (i = 0; i < 480 * 320; i++)
